3d6df7bb57735df88d3a0b28f33e76a987ad7904
Valentino Ekaputra <silentrazgriz@gmail.com>
2019-09-02 15:34:30 +0700

    fix: return RunScript fileName dynamically on error

--- cli/util/util.go
+++ cli/util/util.go
@@ -13,25 +13,25 @@ func RunScript(fileName string, content string, args ...string) error {
 	// Create sh file
 	initFile, err := os.Create(fileName)
 	if nil != err {
-		return errors.New(fmt.Sprintf("failed to create tango-init.sh: %s", err))
+		return errors.New(fmt.Sprintf("failed to create %s: %s", fileName, err))
 	}
 
 	// Write script to sh file
 	_, err = initFile.Write([]byte(content))
 	if nil != err {
-		return errors.New(fmt.Sprintf("failed to write tango-init.sh: %s", err))
+		return errors.New(fmt.Sprintf("failed to write %s: %s", fileName, err))
 	}
 
 	// Close file after writing
 	err = initFile.Close()
 	if nil != err {
-		return errors.New(fmt.Sprintf("failed to close file tango-init.sh: %s", err))
+		return errors.New(fmt.Sprintf("failed to close file %s: %s", fileName, err))
 	}
 
 	// Set chmod to 755
 	err = os.Chmod(fileName, 0755)
 	if nil != err {
-		return errors.New(fmt.Sprintf("failed to chmod tango-init.sh: %s", err))
+		return errors.New(fmt.Sprintf("failed to chmod %s: %s", fileName, err))
 	}
 
 	cmd := exec.Command(fmt.Sprintf("./%s", fileName), args...)
@@ -42,11 +42,11 @@ func RunScript(fileName string, content string, args ...string) error {
 	// Remove sh file after running
 	rErr := os.Remove(fileName)
 	if nil != rErr {
-		return errors.New(fmt.Sprintf("failed to remove tango-init.sh: %s", err))
+		return errors.New(fmt.Sprintf("failed to remove %s: %s", fileName, err))
 	}
 
 	if nil != err {
-		return errors.New(fmt.Sprintf("failed to run tango-init.sh: %s", err))
+		return errors.New(fmt.Sprintf("failed to run %s: %s", fileName, err))
 	}
 
 	return nil
