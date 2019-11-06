633be9fbf610cc29ec57c4fea198387fc57a4ed8
Valentino Ekaputra <silentrazgriz@gmail.com>
2019-11-06 13:27:41 +0700

    enhancement: change migration outside of app deployment

--- cmd/tango/main.go
+++ cmd/tango/main.go
@@ -1,21 +1,12 @@
 package main
 
 import (
-	"github.com/payfazz/go-apt/pkg/fazzdb"
 	"github.com/payfazz/tango/config"
-	"github.com/payfazz/tango/database/migration"
 	"github.com/payfazz/tango/http/server"
 )
 
 func main() {
-	fazzdb.Migrate(config.GetMigrateDb(),
-		"go-backend",
-		config.ForceMigrate(),
-		config.RunSeeder(),
-		migration.Sequence...,
-	)
 	config.SetVerboseQuery()
-
 	api := server.CreateApiServer()
 	api.Serve()
 }
