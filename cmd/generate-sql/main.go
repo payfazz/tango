66e762dc4355db8261ab45be218c963e84ef3a2e
Valentino Ekaputra <silentrazgriz@gmail.com>
2019-12-23 20:41:45 +0700

    feature: generate sql migration from fazzdb migration

---
+++ cmd/generate-sql/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+
+	"github.com/payfazz/go-apt/pkg/fazzdb"
+	"github.com/payfazz/tango/database/migration"
+)
+
+const (
+	SQL_DIR           = "./sql"
+	SQL_FORMATTER_CLI = "sql-formatter-cli"
+)
+
+func main() {
+	fmt.Println("generating sql migration files")
+
+	if !isSQLFormatterCLIExist() {
+		panic("sql-formatter-cli not found: please install sql-formatter-cli before continue; 'npm install -g sql-formatter-cli'")
+	}
+
+	queries := fazzdb.Raw(true, migration.Sequence...)
+
+	err := os.MkdirAll(SQL_DIR, os.FileMode(0744))
+	if nil != err {
+		panic(err)
+	}
+
+	for i, v := range queries {
+		generatedFile := fmt.Sprintf("%s/%d.sql", SQL_DIR, i+1)
+
+		_, err = os.Stat(generatedFile)
+		if !os.IsNotExist(err) {
+			fmt.Println("file", generatedFile, "already exists, skip generating current migration")
+			continue
+		}
+
+		err = ioutil.WriteFile(generatedFile, []byte(v), os.FileMode(0644))
+		if nil != err {
+			panic(err)
+		}
+
+		cmd := exec.Command(
+			SQL_FORMATTER_CLI,
+			"-i", generatedFile, "-o", generatedFile,
+		)
+
+		if err = cmd.Start(); nil != err {
+			fmt.Println("ERROR: failed formatting file", generatedFile)
+			continue
+		}
+	}
+
+	fmt.Println("finish generate migration into sql files")
+}
+
+func isSQLFormatterCLIExist() bool {
+	_, err := exec.LookPath(SQL_FORMATTER_CLI)
+	return nil == err
+}
--- go.mod
+++ go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/mattn/go-sqlite3 v1.11.0 // indirect
 	github.com/nats-io/jwt v0.2.12 // indirect
 	github.com/payfazz/fazzcard-backend v0.6.0
-	github.com/payfazz/go-apt v0.16.4
+	github.com/payfazz/go-apt v0.17.0
 	github.com/payfazz/go-middleware v0.0.0-20190724100055-2ef25bcd2c13 // indirect
 	github.com/payfazz/go-router v0.0.0-20190720102111-42516499ed6e // indirect
 	github.com/pkg/errors v0.8.1
