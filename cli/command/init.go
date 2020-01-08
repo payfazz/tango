d029c0c4eddbf34ad46342876a6e5a296b489bc2
Valentino Ekaputra <silentrazgriz@gmail.com>
2020-01-08 16:09:01 +0700

    fix: different sed for mac and linux

--- cli/command/init.go
+++ cli/command/init.go
@@ -2,9 +2,9 @@ package command
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/payfazz/tango/cli/util"
-
 	"github.com/urfave/cli"
 )
 
@@ -14,7 +14,7 @@ Usage: tango init my-new-project`
 
 var invalidProjectName = `Invalid project name, 'test' cannot be used for project name!`
 
-var initScripts = `#!/bin/sh
+var initLinuxScripts = `#!/bin/sh
 
 set -x
 
@@ -23,6 +23,19 @@ cd $1
 mv cmd/tango cmd/$1
 find .ci cmd config database http internal lib test -type f -exec sed -i'' "s/tango/$1/g" {} \;
 sed -i'' "s/tango/$1/g" go.mod
+go mod tidy
+rm -rf cli cli.go .git
+cd ..`
+
+var initMacScripts = `#!/bin/sh
+
+set -x
+
+git clone git@github.com:payfazz/tango.git $1
+cd $1
+mv cmd/tango cmd/$1
+find .ci cmd config database http internal lib test -type f -exec sed -i '' "s/tango/$1/g" {} \;
+sed -i '' "s/tango/$1/g" go.mod
 go mod tidy
 rm -rf cli cli.go .git
 cd ..`
@@ -36,6 +49,8 @@ func InitCommand() cli.Command {
 		Aliases: []string{"i"},
 		Usage:   "tango init <project_name>",
 		Action: func(c *cli.Context) {
+			var err error
+
 			projectName := c.Args().Get(0)
 			if "" == projectName {
 				fmt.Println(missingProjectName)
@@ -47,7 +62,12 @@ func InitCommand() cli.Command {
 				return
 			}
 
-			err := util.RunScript(fileName, initScripts, projectName)
+			if runtime.GOOS == "darwin" { // mac
+				err = util.RunScript(fileName, initMacScripts, projectName)
+			} else { // linux
+				err = util.RunScript(fileName, initLinuxScripts, projectName)
+			}
+
 			if nil != err {
 				fmt.Println(err)
 				return
